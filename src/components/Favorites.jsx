7bc035aba3053a7edd44bd5c947a33c97a1e30c5
Amanuel <beginningoflife2324@gmail.com>
2025-11-02 10:34:18 -0900

    initialised the file

---
+++ src/components/Favorites.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Favorites = () => {
+  return (
+    <div>
+      <p>Hello this is ur favorites page</p>
+    </div>
+  )
+}
+
+export default Favorites;
---
+++ src/components/SignUp.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const SignUp = () => {
+  return (
+    <div>
+      <p>helloo this is ur sign up page</p>
+    </div>
+  )
+}
+
+export default SignUp;
