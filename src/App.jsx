b89e049ce6fc31ac6371cb253a1f1fee92fe466b
Amanuel <beginningoflife2324@gmail.com>
2025-11-02 10:31:48 -0900

    route the paths and navigations

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,34 @@
 import React from 'react';
-
+import Navbar from './components/Navbar';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css'
+import ProductCard from './components/ProductCard';
+import HomePage from './pages/HomePage';
+import Cart from './pages/Cart';
+import SignIn from './components/SignIn';
+import SignUp from './components/SignUp';
+
+import Footer from './components/Footer';
+import Favorites from './components/Favorites';
+
 
 function App() {
   
 
   return (
     <>
-    <p className="bg-blue-600"> boom</p>
+    <Router>
+      <Navbar/>
+     <Routes>
+      <Route path="/" element={<HomePage />} />
+      <Route path="/cart" element={<Cart />} />
+      <Route path="/signin" element={<SignIn />} />
+      <Route path="/signup" element={<SignUp />} />
+      <Route path="/favorites" element={<Favorites />} />
+
+     </Routes>
+      <Footer />
+    </Router>
      </>
   )
 }
