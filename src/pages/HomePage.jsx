6927215bd572d8d4c4ad4e0109bab33696d98ce1
Amanuel <beginningoflife2324@gmail.com>
2025-11-02 10:35:04 -0900

    homepages of productcard and header part

---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import ProductCard from '../components/ProductCard';
+import HeroSection from '../components/HeroSection';
+
+const HomePage = () => {
+  return (
+    <div>
+        <HeroSection />
+      <ProductCard /> 
+    </div>
+  )
+}
+
+export default HomePage;
